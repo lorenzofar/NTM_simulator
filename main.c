23b8392208c2e317f00ad2541ec389cd22b35d41
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-07-07 00:22:23 +0200

    Add parsing function
    Finished parsing function using a vector to access states.
    Added helper function to visualize inserted transitions

--- main.c
+++ main.c
@@ -1,56 +1,107 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
 #define CONFIG_DIM 3
 
-// Define structures to represent states
+// Useful definitions
 typedef enum{
-    L, R, S
-} direction;
+    TRUE, FALSE
+} bool;
 
-typedef struct state{
-    short n;
+// Transition representation
+typedef struct transition{
+    int target;
     char in;
     char out;
-    direction move;
-    short target;
-    struct state *children;
-    struct state *sibling;
+    char move;
+    struct transition *next;
+} transition;
+
+// State representation
+typedef struct state{
+    bool final;
+    transition *transitions;
 } state;
 
-//Define structures to represent tape
-typedef struct tape_cell{
-    char val;
-    struct tape_cell *prev;
-    struct tape_cell *next;
-} tape_cell;
+// Table to store data
+typedef struct{
+    int size;
+    state *states;
+} ht;
 
-tape_cell TAPE; 
-
+// Functions prototypes
 void parse();
+void visualizeTransitions();
+
+ht MT;
 
 int main(){
-    //Initialize tape
-    
-    //Start reading input
-    //Because the different parts are delimited by tr / acc / max, I just use a temporary string of lenght 3
-    char temp[CONFIG_DIM];
-    scanf("%s", temp);
-    printf("%s", temp);
+    char temp_input[CONFIG_DIM];
+    scanf("%s", temp_input);
     parse();
-    scanf("%s", temp);
-    printf("%s", temp);
+    visualizeTransitions();
+    scanf("%s", temp_input);
     return 0;
 }
 
 void parse(){
-    int t_start, t_end, i, scan_result;
+    int t_start, t_end, scan_result, temp, temp_char, old_size;
     char t_in, t_out, t_mv;
-    printf("PARSING\n");
-    scan_result = scanf("%d %c %c %c %d", &t_start, &t_in, &t_out, &t_mv, &t_end);
+    transition *temp_tr, *new_tr;
+    // Initialize MT
+    MT.size = 1;
+    MT.states = (state *)malloc(sizeof(state));
+    MT.states[0].transitions = NULL;
+
+    // Read input until end
+    scan_result = scanf("%d %c %c %c %d", &t_start, &t_in, &t_out, &t_mv, &t_end);    
     while(scan_result){
-        printf("%d %c %c %c %d\t scanf result: %d\n", t_start, t_in, t_out, t_mv, t_end, scan_result);
+
+        // Check if the size of the ht is enough
+        temp = t_start + 1;
+        if(temp > MT.size){
+            old_size = MT.size;
+            MT.size = temp;
+            MT.states = (state *)realloc(MT.states, temp * sizeof(state));
+            // Initialize transitions to null
+            for(old_size; old_size < MT.size; old_size++)
+                MT.states[old_size].transitions = NULL;
+        }       
+
+        // Create new transition according to input
+        new_tr = (transition *)malloc(sizeof(transition));
+        new_tr->in = t_in;
+        new_tr->out = t_out;
+        new_tr->move = t_mv;
+        new_tr->target = t_end;
+        new_tr->next = NULL;
+        
+        // Check if already exists a state
+        temp_tr = MT.states[t_start].transitions;
+        if(temp_tr != NULL);
+            new_tr->next = temp_tr;
+
+        // Save data
+        MT.states[t_start].final = FALSE;
+        MT.states[t_start].transitions = new_tr;
+        
+        // Read input
         scan_result = scanf("%d %c %c %c %d", &t_start, &t_in, &t_out, &t_mv, &t_end);   
+    }
+}
+
+void visualizeTransitions(){
+    int i, j;
+    transition *temp;
+    for(i=0; i<MT.size; i++){
+        printf("State %d:\n", i);
+        temp = MT.states[i].transitions;
+        j = 0;
+        while(temp != NULL){
+            printf("tr%d: %c %c %c %d\n", j, temp->in, temp->out, temp->move, temp->target);
+            temp = temp->next;
+            j++;
+        }
+        printf("\n");
     }
 }
